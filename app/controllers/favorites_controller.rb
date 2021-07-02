41b8f6a1e7e17f8a0b073b20e24f3b2c0f66452d
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-07-02 09:22:47 +0000

    [Add]6-3_comments.count

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
-  
+
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
@@ -8,5 +8,9 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_to book_path(book)
   end
 end
